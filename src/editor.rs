256ee652bfd61dba43bc76b03a8766d9c3f1191b
khammersmith17 <kilianhammersmith@gmail.com>
2024-08-13 09:18:57 -0400

    moved all terminal functionality to its own struct

--- src/editor.rs
+++ src/editor.rs
@@ -1,53 +1,34 @@
 use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
-use crossterm::terminal::{size, enable_raw_mode, disable_raw_mode, Clear, ClearType};
-use crossterm::execute;
-use crossterm::cursor::MoveTo;
-use std::io::stdout;
-
+mod terminal;
+use terminal::Terminal;
 
 pub struct Editor{
     should_quit: bool,
 }
 
 impl Editor {
-    pub fn new() -> Self {
+    pub const fn new() -> Self {
         Editor{
             should_quit: false
         }
     }
 
     pub fn run(&mut self){
-        Self::initialize().unwrap();
-        Self::draw_rows().unwrap();
+        Terminal::initialize().unwrap();
         let result = self.repl();
-        Self::terminate().unwrap();
+        Terminal::terminate().unwrap();
         result.unwrap();
     }
 
-    fn initialize() -> Result<(), std::io::Error> {
-        enable_raw_mode()?;
-        Self::clear_screen()
-    }
-
-    fn terminate() -> Result<(), std::io::Error> {
-        disable_raw_mode()
-    }
-
-    fn clear_screen() -> Result<(), std::io::Error> {
-        let mut stdout = stdout();
-        execute!(stdout, Clear(ClearType::All))
-    }
     fn repl(&mut self) -> Result<(), std::io::Error> {
         loop {
-            let event = read()?;
-            self.evaluate_event(&event);
             self.refresh_screen()?;
-
             if self.should_quit {
                 break;
             }
+            let event = read()?;
+            self.evaluate_event(&event);
         }
-
         Ok(())
     }
 
@@ -66,19 +47,22 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         if self.should_quit {
-            Self::clear_screen()?;
+            Terminal::clear_screen()?;
             println!("Goodbye.\r\n");
+        } else {
+            Self::draw_rows()?;
+            Terminal::move_cursor_to(0,0)?;
         }
         Ok(())
     }
 
     fn draw_rows() -> Result<(), std::io::Error> {
-        let size = size()?;
-        let mut stdout = stdout();
-        let column: u16 = 0;
-        for i in 1..size.0 {
-            execute!(stdout, MoveTo(column, i))?;
+        let height = Terminal::size()?.1;
+        for row in 0..height {
             print!("~");
+            if row + 1 <  height {
+                print!("\r\n");
+            }
         }
         Ok(())
     }
